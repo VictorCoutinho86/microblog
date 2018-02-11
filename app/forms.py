09f935d1924a9dab3fb293079f66e95ebcf3d35c
VictorCoutinho86 <victormelo@id.uff.br>
2018-02-10 22:05:16 -0200

    Adding some style to Login page and Posts include!

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,9 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
 
